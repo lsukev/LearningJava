185dec76f83a98dd8eb7a54687f0c76bfa88e071
Kevin Ivy <kevinivy1@gmail.com>
2016-08-04 13:08:31 -0500

    Completed Strings Tutorial for Java

---
+++ learningjava2/src/main/java/com/example/Strings.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Strings {
+    public static void main(String[] args) {
+        String vehicle = "Truck";
+        String make = "Chevy";
+        String model = "Tahoe";
+
+        String fullVehicleDetails = vehicle + " - " + make + " " + model;
+
+        System.out.println(fullVehicleDetails);
+
+        String upper = "CRAZY BEAST!";
+        String lower = upper.toLowerCase();
+
+        System.out.println(lower);
+
+        int accountBalance = 500;
+
+        String printBalance = String.format("Your account balance is %d", accountBalance);
+
+        System.out.println(printBalance);
+    }
+}
+
