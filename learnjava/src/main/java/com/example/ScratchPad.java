d76543d2ce79d072528286f529d1bce6d7e722ee
Kevin Ivy <kevinivy1@gmail.com>
2016-08-04 12:32:34 -0500

    Initial Commit

---
+++ learnjava/src/main/java/com/example/ScratchPad.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class ScratchPad {
+    public static void main(String[] args) {
+
+        //01010101010001 000111000
+        String name = "John";
+        int homeRuns = 55;
+        float bankBalance = 100.44f;
+
+        float finalBalance = bankBalance - 50;
+
+        System.out.println(name + " has " + finalBalance + " in his account!");
+    }
+}
