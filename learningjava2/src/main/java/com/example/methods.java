34be9909a7af50da1ec63480637f7734640ffa75
Kevin Ivy <kevinivy1@gmail.com>
2016-08-05 10:13:29 -0500

    Finished Methods Tutorial

--- learningjava2/src/main/java/com/example/methods.java
+++ learningjava2/src/main/java/com/example/methods.java
@@ -6,6 +6,22 @@ package com.example;
 public class methods {
 
     public static void main(String[] args) {
+        printName();
+        printArea(10,5);
+        int area = getArea(5,6);
+    }
 
+    public static void printName() {
+
+        System.out.println("Hi, my name is Kevin but my friends call me kev!");
+    }
+
+    public static void printArea(int length, int width){
+        System.out.println("Area: " + (length * width + 1));
+    }
+
+    public static int getArea(int length, int width){
+        return length * width;
     }
 }
+
